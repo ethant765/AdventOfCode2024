404619695c3fd12384fc1f64aa4b654a802bedbc
Ethan Thompson <ethant765@gmail.com>
2025-12-07 07:51:54 +0000

    Day 6 challenge 1 complete

--- Day_6/include/resource_handler.h
+++ Day_6/include/resource_handler.h
@@ -4,42 +4,161 @@
 #include <vector>
 #include <string>
 #include <cstdint>
+#include <utility>
 
 namespace day_six {
-
+    /**
+     * @brief day 6 resource holder and processor
+     */
     class ResourceHandler {
         public:
+            /**
+             * @brief Construct a new default Resource Handler object
+             */
             ResourceHandler() = default;
+
+            /**
+             * @brief Destroy the Resource Handler object
+             */
             ~ResourceHandler() = default;
+
+            /**
+             * @brief Adds a new line to the map, a new line will contain new x axis positions
+             * 
+             * @param new_map_line 
+             */
             void AddLineToMap(const std::string& new_map_line);
+
+            /**
+             * @brief Calculates and counts the number of map positions the guard may visit
+             * 
+             * @return uint64_t 
+             */
             uint64_t CountDistinctPositions();
             
         private:
+            /**
+             * @brief ENUM to store the different directions a guard can visit
+             */
             enum POSSIBLE_DIRECTIONS {
-                UP,
-                LEFT,
+                UP = 0,
                 RIGHT,
                 DOWN,
+                LEFT,
                 UNKNOWN
             };
-            std::vector<std::vector<char>> map_;
-            std::vector<std::vector<char>> visited_places_map_;
+            
+            /**
+             * @brief Handles the position predictions for the guards route
+             */
             void PredictPositions();
 
-            bool CanGuardMove(const size_t pos_x, const size_t pos_y, const POSSIBLE_DIRECTIONS direction);
-            bool CanGuardExit(const size_t pos_x, const size_t pos_y, const POSSIBLE_DIRECTIONS direction);
-            POSSIBLE_DIRECTIONS RotateGuard(const POSSIBLE_DIRECTIONS current_direction);
+            /**
+             * @brief Given the guards location and facing direction, will advise if the guard can move forwards
+             * 
+             * @param pos_x 
+             * @param pos_y 
+             * @param direction 
+             * @return true 
+             * @return false 
+             */
+            bool CanGuardMove(const long int pos_x, const long int pos_y, const POSSIBLE_DIRECTIONS direction);
 
+            /**
+             * @brief Given the guards location and facing direction, will advise if the guard can exit the map
+             * 
+             * @param pos_x 
+             * @param pos_y 
+             * @param direction 
+             * @return true 
+             * @return false 
+             */
+            bool CanGuardExit(const long int pos_x, const long int pos_y, const POSSIBLE_DIRECTIONS direction);
+
+            /**
+             * @brief Given the guards current direction, will predict the new direction they shall patroll
+             * 
+             * @param direction 
+             * @return POSSIBLE_DIRECTIONS 
+             */
+            POSSIBLE_DIRECTIONS RotateGuard(const POSSIBLE_DIRECTIONS direction);
+
+            /**
+             * @brief Given the guards current position and direction, will return their next predicted location
+             * 
+             * @param pos_x 
+             * @param pos_y 
+             * @param direction 
+             * @return std::pair<long int, long int> 
+             */
+            std::pair<long int, long int> GetNewIndexes(const long int pos_x, const long int pos_y, const POSSIBLE_DIRECTIONS direction);
+
+            /**
+             * @brief given a direction, will return a map marker showing the way the guard is facing
+             * 
+             * @param direction 
+             * @return char 
+             */
+            char GetDirectionIcon(const POSSIBLE_DIRECTIONS direction) const;
+
+            /**
+             * @brief Stores the map area the guard is patrolling
+             */
+            std::vector<std::vector<char>> map_;
+
+            /**
+             * @brief Stores the map with the locations the guard has been to
+             */
+            std::vector<std::vector<char>> visited_places_map_;
+
+            /**
+             * @brief Icon for locations visited
+             */
             const char kPlaceholderVistedMarker = 'X';
+
+            /**
+             * @brief Icon for empty map location
+             */
             const char kPlaceholderEmptyMarker = '.';
+
+            /**
+             * @brief Icon for a map obstacle
+             */
             const char kObstacleMarker = '#';
+
+            /**
+             * @brief Icon for guard facing up
+             */
             const char kFacingUpwardMarker = '^';
+
+            /**
+             * @brief Icon for guard facing down
+             */
             const char kFacingDownMarker = 'V';
+
+            /**
+             * @brief Icon for guard facing left
+             */
             const char kFacingLeftMarker = '<';
+
+            /**
+             * @brief Icon for guard facing right
+             */
             const char kFacingRightMarker = '>';
 
-            size_t current_pos_x = 0;
-            size_t current_pos_y = 0;
+            /**
+             * @brief Stores the current known x axis position
+             */
+            long int current_pos_x = 0;
+
+            /**
+             * @brief Stores the current known y axis position
+             */
+            long int current_pos_y = 0;
+
+            /**
+             * @brief Stores the current known direction
+             */
             POSSIBLE_DIRECTIONS current_direction = POSSIBLE_DIRECTIONS::UNKNOWN;
     };
 
--- Day_6/src/resource_handler.cc
+++ Day_6/src/resource_handler.cc
@@ -4,13 +4,14 @@
 #include <vector>
 #include <iostream>
 #include <cstdint>
+#include <utility>
 
 namespace day_six {
     void ResourceHandler::AddLineToMap(const std::string& new_map_line) {
         std::vector<char> new_line (new_map_line.begin(), new_map_line.end());
         std::vector<char> placeholder_map;
 
-        for(size_t i = 0; i < new_line.size(); i++) {
+        for(long int i = 0; i < new_line.size(); i++) {
             const char s = new_line[i];
             if(s == kFacingUpwardMarker || s == kFacingDownMarker || s == kFacingLeftMarker || s == kFacingRightMarker) {
                 if (current_direction == POSSIBLE_DIRECTIONS::UNKNOWN) {
@@ -44,8 +45,8 @@ namespace day_six {
         PredictPositions();
         uint64_t position_count = 0;
 
-        for(size_t y = 0; y < visited_places_map_.size(); y++) {
-            for(size_t x = 0; x < visited_places_map_[y].size(); x++) {
+        for(long int y = 0; y < visited_places_map_.size(); y++) {
+            for(long int x = 0; x < visited_places_map_[y].size(); x++) {
                 if(visited_places_map_[y][x] == kPlaceholderVistedMarker) {
                     position_count++;
                 }
@@ -58,67 +59,129 @@ namespace day_six {
     void ResourceHandler::PredictPositions() {
         bool exited_map = false;
 
+        uint counter = 0;
         while(!exited_map) {
+            counter++;
+            //std::cout << "Loop #" << counter << ", x: " << current_pos_x << ", y: " << current_pos_y << " direction: " << current_direction << std::endl;
+
             if (CanGuardExit(current_pos_x, current_pos_y, current_direction)) {
                 exited_map = true;
             } else if (CanGuardMove(current_pos_x, current_pos_y, current_direction)) {
                 // handle moving the position of x & y
                 // plot position in placeholder map
-                switch(current_direction) {
-                    case POSSIBLE_DIRECTIONS::DOWN: {
-                        current_pos_y = current_pos_y + 1;
-                        break;
-                    }
-                    case POSSIBLE_DIRECTIONS::LEFT: {
-                        current_pos_x = current_pos_x - 1;
-                        break;
-                    }
-                    case POSSIBLE_DIRECTIONS::RIGHT: {
-                        current_pos_x = current_pos_x + 1;
-                        break;
-                    }
-                    case POSSIBLE_DIRECTIONS::UP {
-                        current_pos_y = current_pos_y - 1;
-                        break;
-                    }
+                std::pair<long int, long int> new_indexes = GetNewIndexes(current_pos_x, current_pos_y, current_direction);
+                if(new_indexes.first == -1 || new_indexes.first == map_[current_pos_y].size() || new_indexes.second == -1 || new_indexes.second == map_.size()) {
+                    exited_map = true;
+                    std::cout << "ERROR: Guard location unknown, unable to move!" << std::endl;
+                } else {
+                    // move marker in map, and put as known position in visited map
+                    map_[current_pos_y][current_pos_x] = kPlaceholderEmptyMarker;
+                    current_pos_x = new_indexes.first;
+                    current_pos_y = new_indexes.second;
+                    map_[current_pos_y][current_pos_x] = GetDirectionIcon(current_direction);
+                    visited_places_map_[current_pos_y][current_pos_x] = kPlaceholderVistedMarker;
                 }
-                visited_places_map_[current_pos_y][current_pos_x] = kPlaceholderVistedMarker;
             } else {
                 current_direction = RotateGuard(current_direction);
             }
         }
     }
 
-    bool ResourceHandler::CanGuardMove(const size_t pos_x, const size_t pos_y, const ResourceHandler::POSSIBLE_DIRECTIONS direction) {
-        try {
-            switch(current_direction) {
-                case POSSIBLE_DIRECTIONS::DOWN: {
-                    
-                }
-                case POSSIBLE_DIRECTIONS::LEFT: {
-                    
-                    break;
-                }
-                case POSSIBLE_DIRECTIONS::RIGHT: {
-                    
-                    break;
-                }
-                case POSSIBLE_DIRECTIONS::UP {
-                    
-                    break;
-                }
-            }
-        } catch(...) {
-            // index out of bounds, cannot move!
+    bool ResourceHandler::CanGuardMove(const long int pos_x, const long int pos_y, const ResourceHandler::POSSIBLE_DIRECTIONS direction) {
+        std::pair<long int, long int> new_indexes = GetNewIndexes(pos_x, pos_y, direction);
+
+        if(new_indexes.first == -1 || new_indexes.first == map_[pos_y].size() || new_indexes.second == -1 || new_indexes.second == map_.size()) {
             return false;
+        }
+
+        if(map_[new_indexes.second][new_indexes.first] == kPlaceholderEmptyMarker) {
+            return true;
+        }
+        return false;
+    }
+
+    bool ResourceHandler::CanGuardExit(const long int pos_x, const long int pos_y, const ResourceHandler::POSSIBLE_DIRECTIONS direction) {
+        std::pair<long int, long int> new_indexes = GetNewIndexes(pos_x, pos_y, direction);
+        if(new_indexes.first == -1 || new_indexes.second == -1) {
+            std::cout << "ERROR: Guard already left!" << std::endl;
+            return false;
+        }
+
+        if(new_indexes.first == map_[pos_y].size() || new_indexes.second == map_.size()) {
+            return true;
+        }
+        return false;
+    }
+    
+    ResourceHandler::POSSIBLE_DIRECTIONS ResourceHandler::RotateGuard(const ResourceHandler::POSSIBLE_DIRECTIONS direction) {
+        switch(direction) {
+            case POSSIBLE_DIRECTIONS::DOWN: {
+                return POSSIBLE_DIRECTIONS::LEFT;
+            }
+            case POSSIBLE_DIRECTIONS::LEFT: {
+                return POSSIBLE_DIRECTIONS::UP;
+            }
+            case POSSIBLE_DIRECTIONS::RIGHT: {
+                return POSSIBLE_DIRECTIONS::DOWN;
+            }
+            case POSSIBLE_DIRECTIONS::UP: {
+                return POSSIBLE_DIRECTIONS::RIGHT;
+            }
+            default: {
+                std::cout << "ERROR: Guard location unknown, unable to move!" << std::endl;
+                return POSSIBLE_DIRECTIONS::UNKNOWN;
+            }
         }
     }
 
-    bool ResourceHandler::CanGuardExit(const size_t pos_x, const size_t pos_y, const ResourceHandler::POSSIBLE_DIRECTIONS direction) {
-
+    std::pair<long int, long int> ResourceHandler::GetNewIndexes(const long int pos_x, const long int pos_y, const ResourceHandler::POSSIBLE_DIRECTIONS direction) {
+        long int new_x = pos_x;
+        long int new_y = pos_y;
+        switch(current_direction) {
+            case POSSIBLE_DIRECTIONS::DOWN: {
+                new_y++;
+                break;
+            }
+            case POSSIBLE_DIRECTIONS::LEFT: {
+                new_x--;
+                break;
+            }
+            case POSSIBLE_DIRECTIONS::RIGHT: {
+                new_x++;
+                break;
+            }
+            case POSSIBLE_DIRECTIONS::UP: {
+                new_y--;
+                break;
+            }
+            default: {
+                std::cout << "Guard direction unknown!" << std::endl;
+                new_y = -1;
+                new_x = -1;
+                break;
+            }
+        }
+        return std::pair<long int, long int>(new_x, new_y);
     }
-    
-    ResourceHandler::POSSIBLE_DIRECTIONS ResourceHandler::RotateGuard(const ResourceHandler::POSSIBLE_DIRECTIONS current_direction) {
 
+    char ResourceHandler::GetDirectionIcon(const ResourceHandler::POSSIBLE_DIRECTIONS direction) const {
+        switch(current_direction) {
+            case POSSIBLE_DIRECTIONS::UP: {
+                return kFacingUpwardMarker;
+            }
+            case POSSIBLE_DIRECTIONS::DOWN: {
+                return kFacingDownMarker;
+            }
+            case POSSIBLE_DIRECTIONS::LEFT: {
+                return kFacingLeftMarker;
+            }
+            case POSSIBLE_DIRECTIONS::RIGHT: {
+                return kFacingRightMarker;
+            }
+            default: {
+                std::cout << "ERROR: location unknown, placing marker: " << kPlaceholderEmptyMarker << " on map for current position!" << std::endl;
+                return kPlaceholderEmptyMarker;
+            }
+        }
     }
-}
+} // namespace day_six
