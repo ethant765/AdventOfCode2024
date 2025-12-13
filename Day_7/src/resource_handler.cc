b0563a689e34af95f5659a3227cd9b9715bfdb67
Ethan Thompson <ethant765@gmail.com>
2025-12-13 16:29:50 +0000

    challenge 1 bug - fixing it later

--- Day_7/src/resource_handler.cc
+++ Day_7/src/resource_handler.cc
@@ -75,10 +75,10 @@ namespace day_seven {
 
                         if((i+1) < resource.values.size()) {
                             if((num_x_added < number_of_x) && (num_x_added > 0 || index_to_start_x == i)) {
-                                calculation << "*";
+                                calculation << kMultiplyOperator;
                                 num_x_added++;
                             } else {
-                                calculation << "+";
+                                calculation << kPlusOperator;
                             }
                         }
                     }
@@ -97,7 +97,93 @@ namespace day_seven {
     }
 
     uint64_t ResourceHandler::GetSumFromString(const std::string& calculation) {
-        return 0;
+        size_t prev_index = 0;
+        std::vector<uint32_t> values;
+        std::vector<char> operators;
+
+        try {
+            for(size_t c = 0; c < calculation.size(); c++) {
+                if(calculation[c] == kMultiplyOperator || calculation[c] == kPlusOperator) {
+                    operators.push_back(calculation[c]);
+                    values.push_back(std::stol(calculation.substr(prev_index, c)));
+                    prev_index = c+1;
+                }
+            }
+            // add final number
+            values.push_back(std::stol(calculation.substr(prev_index)));
+        } catch(std::invalid_argument const& e) {
+            std::cout << "ERROR: Failed parsing calculation values. Results may be inaccurate." << std::endl;
+            return 0;
+        } catch(std::out_of_range const& e) {
+            std::cout << "ERROR: Failed parsing calculation values. Results may be inaccurate." << std::endl;
+            return 0;
+        }
+
+        // verify we have exactly 1 more integer than we do operators
+        if(operators.size() != (values.size()-1)) {
+            std::cout << "ERROR: incorrectly parsed operators and values when performing calculation. Results may be inaccurate." << std::endl;
+            return 0;
+        }
+
+        // DO NOT DO MULTIPLICATIONS FIRST - AOC rules are left to right ignoring BODMAS
+        /*bool no_x = false;
+        while(!operators.empty()) {
+            bool mul_found = false;
+            for(size_t i = 0; i < operators.size(); i++) {
+                if(!no_x) {
+                    if(operators[i] == kMultiplyOperator) {
+                        uint32_t val_one = values[i];
+                        uint32_t val_two = values[i + 1];
+
+                        values.erase(values.begin() + (i+1));
+                        values[i] = val_one * val_two;
+
+                        operators.erase(operators.begin() + i);
+
+                        //mul_found = true;
+                        break;
+                    } 
+                } else {
+                    uint32_t val_one = values[i];
+                    uint32_t val_two = values[i + 1];
+
+                    values.erase(values.begin() + (i+1));
+                    values[i] = val_one + val_two;
+
+                    operators.erase(operators.begin() + i);
+                }
+            }
+            if(!mul_found) {
+                no_x = true;
+            }
+        }*/
+
+        while(!operators.empty()) {
+            if(operators[0] == kMultiplyOperator) {
+                uint32_t val_one = values[0];
+                uint32_t val_two = values[1];
+
+                values.erase(values.begin() + 1);
+                values[0] = val_one * val_two;
+
+                operators.erase(operators.begin());
+            } else {
+                uint32_t val_one = values[0];
+                uint32_t val_two = values[1];
+
+                values.erase(values.begin() + 1);
+                values[0] = val_one + val_two;
+
+                operators.erase(operators.begin());
+            }
+        }
+
+        if(values.size() != 1) {
+            std::cout << "ERROR: failed performing calculation of: " << calculation << ", results may be invalid." << std::endl;
+            return 0;
+        }
+
+        return values[0];
     }
 
 } // namespace day_seven
